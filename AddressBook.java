f5b4ccd9fcadd3d0da9e8038adad166ea1007d47
InduLekha98 <rindulekha09@gmail.com>
2021-03-15 02:12:11 +0530

    Added:Ability to add a new contacts in address book

--- AddressBook.java
+++ AddressBook.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class AddressBook {
     private String FirstName;
     private String LastName;
@@ -7,6 +11,7 @@ public class AddressBook {
     private String State;
     private int PhoneNumber;
     private String Email;
+    private HashMap contact;
 
     public AddressBook(String FirstName, String LastName, String Address, String City, int Zip,
                            String State, int PhoneNumber, String Email) {
@@ -18,16 +23,70 @@ public class AddressBook {
         this.State = State;
         this.PhoneNumber = PhoneNumber;
         this.Email = Email;
+
+    }
+
+    public AddressBook() {
+    }
+
+    public HashMap storeContacts(HashMap contacts) {
+        ArrayList<String> entry = new ArrayList<String>();
+        entry.add(FirstName);
+        entry.add(LastName);
+        entry.add(Address);
+        entry.add(String.valueOf(Zip));
+        entry.add(City);
+        entry.add(State);
+        entry.add(String.valueOf(PhoneNumber));
+        entry.add(Email);
+        contact.put(FirstName, entry);
+        System.out.println(entry);
+        System.out.println(contact);
+        return contacts;
+    }
+
+    private static void getContact(AddressBook details) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter you first name");
+        details.FirstName = obj.next();
+        System.out.println("Enter you last name");
+        details.LastName = obj.next();
+        obj.nextLine();
+        System.out.println("Enter you Address name");
+        details.Address = obj.nextLine();
+        System.out.println("Enter you zip ");
+        details.Zip = obj.nextInt();
+        System.out.println("Enter you city name");
+        details.City = obj.next();
+        System.out.println("Enter you state name");
+        details.State = obj.next();
+        obj.nextLine();
+        System.out.println("Enter you phone number");
+        details.PhoneNumber = obj.nextInt();
+        obj.nextLine();
+        System.out.println("Enter you email name");
+        details.Email = obj.nextLine();
+    }
+
+    private void editContacts(HashMap contacts) {
+        System.out.println(contacts);
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = obj.next();
+        System.out.println(contact.get(name));
     }
 
     @Override
     public String toString() {
         return FirstName;
     }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
-        AddressBook contacts = new AddressBook("Indu", "Lekha", "abcd",
-                "Hyderabad", 500012,"Telangana",643547894,"indu@gmail.com");
+        AddressBook contacts = new AddressBook();
+        HashMap<String, ArrayList<String>>  = new HashMap<String, ArrayList<String>>();
+        getContact(contacts);
+        HashMap<String, ArrayList<String>> data = contacts.storeContacts(contacts);
+        contacts = data;
         System.out.println(contacts);
     }
 }
